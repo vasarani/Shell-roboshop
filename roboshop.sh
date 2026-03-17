3553dd1a13feb8a49ea8efde2f6304cb6e0478f6
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-17 12:28:02 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,33 +5,34 @@ ami_id="ami-0220d79f3f480ecf5"
 zone_id="Z07217272DEU08NPF6AZQ"
 domain_name="rawsd.in"
 
-
 for instance in $@
 do
- instance_id=$( aws ec2 run-instances \
-      --image-id $ami_id \
-      --instance-type "t3.micro" \
-      --security-group-ids $sg_id \
-      --tag-specifications "ResourceType=instance, Tags=[{Key=Name, Value=$instance}]" \
-      --query 'Instances[0].InstanceId' \
-      --output text )
-     if [ $instance == "frontend" ]; then
+    instance_id=$( aws ec2 run-instances \
+    --image-id $ami_id \
+    --instance-type "t3.micro" \
+    --security-group-ids $sg_id \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [ $instance == "frontend" ]; then
         IP=$(
             aws ec2 describe-instances \
             --instance-ids $instance_id \
-            --query 'Reservations[].Instances[].PublicIPAddress' \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
-        record_name="$domain_name"
+        record_name="$domain_name" # daws88s.online
     else
         IP=$(
             aws ec2 describe-instances \
             --instance-ids $instance_id \
-            --query 'Reservations[].Instances[].PrivateIPAddress' \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
-        )   
-        record_name="$instance.$domain_name" 
-    fi    
+        )
+        record_name="$instance.$domain_name" # mongodb.daws88s.online
+    fi
+
     echo "IP Address: $IP"
      
     aws route53 change-resource-record-sets \

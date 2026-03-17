b6bd00053c1da61527d565451c6d4437c0d51b8b
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-17 14:50:16 +0530

    shell-roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -10,6 +10,7 @@ userid=$(id -u)
 logs_folder="/var/log/shell-roboshop"
 logs_file="$logs_folder/$0.logs"
 script_dir=$PWD
+mongodb_host=mongodb.rawsd.in
 
 if [ $userid -ne 0 ]; then
  echo -e "$R Please run this script with the root user access $N" | tee -a $logs_file
@@ -25,53 +26,6 @@ VALIDATE(){
        echo -e "$2... $G SUCCESS $N" | tee -a $logs_file
     fi      
 }
-
-# dnf module disable nodejs -y &>>$logs_file
-# VALIDATE $? "Disabling NodeJS Default version"
-
-# dnf module enable nodejs:20 -y &>>$logs_file
-# VALIDATE $? "Enabling NodeJS 20"
-
-# dnf install nodejs -y &>>$logs_file
-# VALIDATE $? "Install NodeJS"
-
-# id roboshop &>>$logs_file
-# if [ $? -ne 0 ]; then
-#     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
-#     VALIDATE $? "Creating the system user"
-# else
-#     echo -e "Roboshop user already exist... $Y SKKIPIMG $N"
-# fi 
-
-# mkdir -p /app
-# VALIDATE $? "Creating the app directory"
-
-# curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$logs_file
-# VALIDATE $? "Downloading catalogue code"
-
-# cd /app
-# VALIDATE $? "Moving to app directory"
-
-# rm -r /app/*
-# VALIDATE $? "Removing the existing code"
-
-# unzip /tmp/catalogue.zip &>>$logs_file
-# VALIDATE $? "unzipcatalogue code"
-
-# npm install
-# VALIDATE $? "Installing dependencies"
-
-# cp catalogue.service /etc/systemd/system/catalogue.service
-# VALIDATE $? "Created systemctl service"
-
-# systemctl deamon-reload
-# systemctl enable catalogue
-# systemctl start catalogue
-# VALIDATE $? "Starting and Enabling the catalogue"
-
-
-
-
 
 
 dnf module disable nodejs -y &>>$logs_file
@@ -116,3 +70,8 @@ systemctl daemon-reload
 systemctl enable catalogue  &>>$logs_file
 systemctl start catalogue
 VALIDATE $? "Starting and enabling catalogue"
+
+cp $script_dir/mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-mongosh -y
+
+mongosh --host $mongodb_host </app/db/master-data.js

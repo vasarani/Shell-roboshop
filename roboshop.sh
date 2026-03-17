dc0d9a51340765e9ab46855d918e83a645ae2a4e
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-17 12:25:13 +0530

    shell-roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,10 +2,13 @@
 
 sg_id="sg-0f99207bb9a2cd68c"
 ami_id="ami-0220d79f3f480ecf5"
+zone_id="Z07217272DEU08NPF6AZQ"
+domain_name="rawsd.in"
+
 
 for instance in $@
 do
- instance_id=$(  aws ec2 run-instances \
+ instance_id=$( aws ec2 run-instances \
       --image-id $ami_id \
       --instance-type "t3.micro" \
       --security-group-ids $sg_id \
@@ -19,13 +22,42 @@ do
             --query 'Reservations[].Instances[].PublicIPAddress' \
             --output text
         )
+        record_name="$domain_name"
     else
         IP=$(
             aws ec2 describe-instances \
             --instance-ids $instance_id \
             --query 'Reservations[].Instances[].PrivateIPAddress' \
             --output text
-        )    
+        )   
+        record_name="$instance.$domain_name" 
     fi    
     echo "IP Address: $IP"
+     
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $zone_id \
+    --change-batch '
+    {
+        "Comment": "Updating record",
+        "Changes": [
+            {
+            "Action": "UPSERT",
+            "ResourceRecordSet": {
+                "Name": "'$record_name'",
+                "Type": "A",
+                "TTL": 1,
+                "ResourceRecords": [
+                {
+                    "Value": "'$IP'"
+                }
+                ]
+            }
+            }
+        ]
+    }
+    '
+
+    echo "record updated for $instance"
+
+
 done
